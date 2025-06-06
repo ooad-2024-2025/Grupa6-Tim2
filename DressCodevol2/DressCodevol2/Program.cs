19d931adb38e857210f18b9ad9361278e04bb895
Ilma Karahodža <ikarahodza1@etf.unsa.ba>
2025-06-06 07:42:11 +0200

    Neke izmjene

--- DressCodevol2/DressCodevol2/Program.cs
+++ DressCodevol2/DressCodevol2/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<Korisnik>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<Korisnik>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
