3c96b4719df75400a74b2c8bbb7dcc37e30b2996
Ilma Karahodža <ikarahodza1@etf.unsa.ba>
2025-06-08 20:13:04 +0200

    random files

--- DressCodevol2/DressCodevol2/obj/Debug/net8.0/DressCode.AssemblyInfo.cs
+++ DressCodevol2/DressCodevol2/obj/Debug/net8.0/DressCode.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DressCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27216ffe16959b41e4bd78abe8e2d2bfc796d8ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94aaa3255790bbe74c7bd6de72570a150c085ba3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DressCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DressCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
