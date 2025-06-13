6f9555578dbc5db41e21c1c7e0676fd37713e160
Ilma Karahodža <ikarahodza1@etf.unsa.ba>
2025-06-13 19:07:10 +0200

    otvorim jednom i trazi commit

--- DressCodevol2/DressCodevol2/obj/Debug/net8.0/DressCode.AssemblyInfo.cs
+++ DressCodevol2/DressCodevol2/obj/Debug/net8.0/DressCode.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DressCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b1be8c85350ae27ef889c84bf710aa1d15c333f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2785a4d1a81020fb7e6099080c0567e7b8449137")]
 [assembly: System.Reflection.AssemblyProductAttribute("DressCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DressCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
