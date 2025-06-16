00282c988d2d66ed742325a2daf990e11f466d71
Ilma Karahodža <ikarahodza1@etf.unsa.ba>
2025-06-16 13:59:50 +0200

    nesto sitno ui

--- DressCodevol2/DressCodevol2/obj/Debug/net8.0/DressCode.AssemblyInfo.cs
+++ DressCodevol2/DressCodevol2/obj/Debug/net8.0/DressCode.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DressCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+924c50d76f6b2617044af19a555d7f38f21ba43c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0fb92a44ac8c98f75a5f2621c4858841ed15304")]
 [assembly: System.Reflection.AssemblyProductAttribute("DressCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DressCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
